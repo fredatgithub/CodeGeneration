48cc3879ff84bf9b4a594e291490a4365260f133
fred <domaine75@gmail.com>
2015-12-31 11:33:47 +0100

    Update FormMain.cs

--- ReplaceStringInManyProject/FormMain.cs
+++ ReplaceStringInManyProject/FormMain.cs
@@ -574,7 +574,7 @@ namespace ReplaceStringInManyProject
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
