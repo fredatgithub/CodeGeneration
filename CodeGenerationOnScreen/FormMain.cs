2dddbd0eb16d07db417a8dbec8e40e8b07052543
fred <domaine75@gmail.com>
2016-01-09 01:12:16 +0100

    Update FormMain.cs

--- CodeGenerationOnScreen/FormMain.cs
+++ CodeGenerationOnScreen/FormMain.cs
@@ -486,7 +486,6 @@ namespace CodeGenerationOnScreen
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
       tb.SelectedText = Clipboard.GetText();
-      //tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
